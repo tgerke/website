c0bf8cc3bd59334af299300f62fe4b76a5736f89
Travis Gerke <tgerke@hsph.harvard.edu>
2017-02-27 20:47:46 -0500

    Add forgotten js to off-canvas overlay

--- travisgerke/js/app.js
+++ travisgerke/js/app.js
@@ -1,1 +1,26 @@
 $(document).foundation()
+
+(function($) {
+
+  var $body = $('body');
+
+  $(document).foundation();
+
+
+  // $(document).on('on.zf.toggler', function(e) {
+
+      $body.on('click.bb.panel', function(e) {
+        var activePanel = $('.simple-panel.is-active');
+
+        if (activePanel.length && !($.contains(activePanel[0], e.target) || activePanel.is(e.target)) && (e.target.getAttribute('data-toggle') !== activePanel[0].id)) {
+          activePanel.foundation('toggle');
+        }
+      });
+    // })
+    // .on('off.zf.toggler', function() {
+    //   console.log('yo!')
+    //   $body.off('click.bb.panel');
+    // });
+
+})(jQuery);
+
