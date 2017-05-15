7363bbfc171249ded75cd443f1c65f410d598103
Travis Gerke <tgerke@hsph.harvard.edu>
2017-05-15 10:23:47 -0400

    swap aerial for hugo-sustain

--- blogdownSetup.R
+++ blogdownSetup.R
@@ -3,11 +3,9 @@ library(devtools)
 devtools::install_github("rstudio/blogdown")
 blogdown::install_hugo()
 
-#http://themes.gohugo.io/aerial/
 setwd("~/Dropbox/websites/website/travisgerke/")
-blogdown::new_site(theme = "sethmacleod/aerial")
 
-
+blogdown::new_site(theme = "nurlansu/hugo-sustain")
 
 
 
