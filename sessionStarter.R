2de6894956fdc9797616dc305dfe797e5840ba3a
Travis Gerke <tgerke@hsph.harvard.edu>
2017-06-05 15:53:43 -0400

    add sessionStarter.R to quickly serve site or create a blog entry

---
+++ sessionStarter.R
@@ -0,0 +1,8 @@
+##############################################################################
+# quick script to launch a new blog entry or serve the site
+ 
+setwd("~/Dropbox/websites/website/travisgerke/")
+
+# make a new blog entry
+blogdown::new_content("blog/2017XXXX/PostName.Rmd", kind="single")
+
