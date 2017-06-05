44876cf18bea27be3028de6c36324e12936c46da
Travis Gerke <tgerke@hsph.harvard.edu>
2017-06-05 15:35:08 -0400

    clean up blogdownSetup.R

--- blogdownSetup.R
+++ blogdownSetup.R
@@ -1,15 +1,31 @@
+##############################################################################
+# package installs
+
 library(devtools)
 devtools::install_github("rstudio/blogdown")
 blogdown::install_hugo()
 
-setwd("~/Dropbox/websites/website/travisgerke/")
+##############################################################################
+# set up main site
 
+# this folder needs to be empty
+setwd("~/Dropbox/websites/website/travisgerke/")
 blogdown::new_site(theme = "nurlansu/hugo-sustain")
 
+# this is equivalent to the Addins -> Serve Site menu dropdown when local=TRUE
 blogdown::build_site(local=TRUE)
+blogdown::build_site(local=FALSE)
 
+# make a new blog entry
+blogdown::new_content("blog/20170522/firstPost.Rmd", kind="single")
+
+# initial build of the publications page; customized in hmtl from there
+blogdown::new_content("publications.Rmd", kind="single")
+
+##############################################################################
+# set up subsite for CV
+
+# needs to be empty
 setwd("~/Dropbox/websites/website/travisgerke/vitae")
-
 blogdown::new_site(theme = "nurlansu/hugo-vitae")
 
-blogdown::new_content("blog/firstPost.Rmd", kind="single")
